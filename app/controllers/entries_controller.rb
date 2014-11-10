b412522c2caf0bf83c60e7d961616c4e5fc0ffcd
Darren Woodley <hodgechen@gmail.com>
2014-11-09 19:57:36 -0500

    fixes behaviour when input field is cleared

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,7 +5,7 @@ class EntriesController < ApplicationController
     def get_results 
       if params[:data]
         key = params[:data][:key]
-        if key.nil?
+        if key.nil? || key.eql?('') 
           results = []
         else
           results = Entry.where('word ILIKE ?', "#{key}%").order('LOWER(word)').limit(10).pluck(:word)
