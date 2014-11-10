9c5b82eda2f3709f160d9f016f49b06076401d45
Darren Woodley <hodgechen@gmail.com>
2014-11-10 12:24:11 -0500

    adds db seed task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Entry.delete_all
+
+f = File.open('/usr/share/dict/web2')
+words = f.readlines.map { |word| word.strip }
+words.each do |word|
+  Entry.create!(word: word)
+end
+
+f.close
